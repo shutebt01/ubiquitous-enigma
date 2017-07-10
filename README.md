0a872817934380b64f60b673736db5f24e319f37
shutebt01 <shutebt01@gmail.com>
2017-07-10 07:27:24 +0000

    Added SCSS parsing to server

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ubiquitous-enigma
+ubiquitous-enigma
+==========
 Human Friendly Symbolic Manipulation Website
+
+Intended to allow for easy manipulation of Sympy
+
+## Site Design
+
+TODO (Neither of us are designers, we are both programmers)
--- server.py
+++ server.py
@@ -1,31 +1,98 @@
 #! /bin/python3
 from sympy import *
 import sympy.plotting as plotting
-import requests, zipfile, os
+import requests, zipfile, os, os.path
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
+from scss import parser
+
+debug=True
 
 port = int(os.getenv("PORT", "80"))
 ip = str(os.getenv("IP", "0.0.0.0"))
 
 print("Preparing to print on {}".format((ip, port)))
 
-def pull_req():
+def pullReq():
     #TODO
     r = requests.get("http://fontawesome.io/assets/font-awesome-4.7.0.zip")
     print(r.text)
 
 
 #if __name__ == "__main__":
-#   pull_req()
+#   pullReq()
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """
     Stolen from https://stackoverflow.com/questions/14088294/multithreaded-web-server-in-python (THANKS BTW)
     """
 
+locCache = dict()
+
+def getLocation(name):
+        global locCache
+        # Intention is to flatten out structure, but to allow for file differentiation
+        rootdir = name.split("/")[1]
+        if rootdir in ["css", "dynamic", "lib", "static"]:
+            # Direct pointer to file
+            return name
+        elif rootdir in locCache:
+            # Cache Generated
+            return locCache[rootdir] + name
+        else:
+            # TODO
+            for i in os.listdir("css"):
+                locCache[i] = "css"
+            for i in os.listdir("dynamic"):
+                locCache[i] = "dynamic"
+            for i in os.listdir("lib"):
+                locCache[i] = "lib"
+            for i in os.listdir("static"):
+                locCache[i] = "static"
+            if rootdir in locCache:
+                return locCache[rootdir] + name
+            else:
+                return None
+
+def compileSCSS(filename):
+    return parser.load(filename)
+
 class IncomingHandler(BaseHTTPRequestHandler):
-    pass
+
+    def returnFile(self, filename, mime=None):
+        if os.path.isfile(filename):
+            self.send_response(200, "OK")
+            if debug:
+                self.send_header("Dev-Test", "File Path: {}".format(filename))
+            if mime:
+                self.send_header("Content-Type", mime)
+            self.end_headers()
+            with open(filename, "rb") as f:
+                senddata = f.read()
+                self.wfile.write(senddata)
+        else:
+            self.send_error(404, "Not File", "The file you requested was a directory")
+            self.end_headers()
+
+    def returnData(self, data, mime=None):
+        self.send_response(200, "OK")
+        if mime:
+            print(mime)
+            self.send_header("Content-Type", mime)
+        self.end_headers()
+        self.wfile.write(data.encode())
+
+    def do_GET(self):
+        truePath = getLocation(self.path)
+        if truePath.lower().endswith("scss"):
+            css = compileSCSS(truePath)
+            self.returnData(css, "text/css")
+        else:
+            self.returnFile(truePath)
+
+
+
 
 if __name__ == "__main__":
-    pass
+    httpd = ThreadedHTTPServer((ip, port), IncomingHandler)
+    httpd.serve_forever()
